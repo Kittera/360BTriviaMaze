0e1e6a777969a0347654b2a0cf9dba6672b52ff8
Steven Omegna <skomegna@uw.edu>
2021-05-11 14:04:11 -0700

    Updated

--- src/main/java/model/QuestionPanel.java
+++ src/main/java/model/QuestionPanel.java
@@ -2,11 +2,9 @@ package model;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;

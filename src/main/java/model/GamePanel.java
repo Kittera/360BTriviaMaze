b5770824d4641f499254d69af76e3dab3c3a734c
Steven Omegna <skomegna@uw.edu>
2021-05-13 09:16:26 -0700

    Updated them and removed some code

--- src/main/java/model/GamePanel.java
+++ src/main/java/model/GamePanel.java
@@ -1,6 +1,57 @@
 package model;
 
-public GamePanel extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    public GamePanel() {
+        super("360 Trivia Maze");
+    }
+
+    public void start() {
+
+        setSize(800, 740);
+
+        final JPanel panel = new JPanel();
+        panel.setBounds(40, 80, 200, 200);
+        panel.setBackground(Color.gray);
+        panel.setLayout(null);
+        add(panel);//add panel to the frame
+
+        // Button1
+        final JButton button1 = new JButton("Click Here");
+        button1.setBounds(50, 100, 95, 30);
+        button1.setBackground(Color.yellow);
+        MainMenu myMenu = new MainMenu();
+        // add(button1); //adds button1 to the frame directly
+        this.setJMenuBar(myMenu.getBar());
+        panel.add(button1); //add button to the panel
+
+        setLocationRelativeTo(null); //centers the frame to windows screen
+        setLayout(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * Main method.
+     *
+     * @param theArgs arguments.
+     */
+    public static void main(final String[] theArgs) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                final GamePanel frame = new GamePanel();
+                frame.start();
+
+            }
+        });
+    }
+
 
     //Build Maze panel here
 }
--- src/main/java/model/MainMenu.java
+++ src/main/java/model/MainMenu.java
@@ -1,8 +1,34 @@
 package model;
 
 
-public class MainMenu extends JFrame {
+import javax.swing.*;
 
+public class MainMenu extends JPanel {
 
+    private static final long serialVersionUID = 3217891232321L;
 
+    JMenuBar mainMenu;
+    JMenu myMenu;
+    JMenuItem startGame, saveGame, loadGame, closeGame;
+
+    public MainMenu() {
+       mainMenu = new JMenuBar();
+       //mainMenu.addMenuListener(new thisMenuListener());
+
+       myMenu = new JMenu("Main Menu");
+       mainMenu.add(myMenu);
+
+       startGame = new JMenuItem("Start Game");
+       myMenu.add(startGame);
+       saveGame = new JMenuItem("Save Game");
+       myMenu.add(saveGame);
+       loadGame = new JMenuItem("Load Game");
+       myMenu.add(loadGame);
+       closeGame = new JMenuItem("Exit Game");
+       myMenu.add(closeGame);
+    }
+
+    public JMenuBar getBar() {
+        return mainMenu;
+    }
 }

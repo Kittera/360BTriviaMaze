cf66401690a6a059c39a5cd98cff2c8a1f1478b3
Kittera <denizine@gmail.com>
2021-05-13 04:02:58 -0700

    Panel factory test file is laid out

---
+++ src/test/kotlin/controller/QuestionPanelFactoryKtTest.kt
@@ -0,0 +1,33 @@
+package controller
+
+import org.junit.jupiter.api.Test
+
+import kotlin.test.fail
+
+internal class QuestionPanelFactoryKtTest {
+
+    @Test
+    fun getQuestionPanel() {
+        fail("Not yet implemented")
+    }
+
+    @Test
+    fun makeTrueFalsePanel() {
+        fail("Not yet implemented")
+    }
+
+    @Test
+    fun makeAnswerPanel() {
+        fail("Not yet implemented")
+    }
+
+    @Test
+    fun makeMultiChoicePanel() {
+        fail("Not yet implemented")
+    }
+
+    @Test
+    fun makeWordAnswerPanel() {
+        fail("Not yet implemented")
+    }
+}
